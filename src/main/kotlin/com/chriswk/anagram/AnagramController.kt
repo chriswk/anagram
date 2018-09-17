83f0cbf4a6c8dbed070f662b4621f7e4859dde51
Christopher Kolstad <christopher.kolstad@finn.no>
2018-09-17 21:49:03 +0200

    Add Crossorigin header

--- src/main/kotlin/com/chriswk/anagram/AnagramController.kt
+++ src/main/kotlin/com/chriswk/anagram/AnagramController.kt
@@ -1,6 +1,7 @@
 package com.chriswk.anagram
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -18,6 +19,7 @@ class AnagramController {
     }
 
     @GetMapping("/anagram", produces = ["application/json"])
+    @CrossOrigin()
     @ResponseBody
     fun getAnagramsFor(@RequestParam("word") word: String): Map<String, List<String>> {
         return mapOf(Pair("anagrams", anagram.anagramsFor(word).values.flatten()))
